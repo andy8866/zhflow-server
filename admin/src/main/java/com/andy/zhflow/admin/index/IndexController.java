f0dab405d7d0e07f8d92218d9317728cfbb00d3b
Andy <Andy.8866@outlook.com>
2023-01-31 12:29:33 +0800

    权限

--- admin/src/main/java/com/andy/zhflow/admin/index/IndexController.java
+++ admin/src/main/java/com/andy/zhflow/admin/index/IndexController.java
@@ -1,12 +1,10 @@
 package com.andy.zhflow.admin.index;
 
 import com.andy.zhflow.response.ResultResponse;
-import jakarta.annotation.security.PermitAll;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@PermitAll()
 @RestController()
 @RequestMapping(value = "/api/admin/index")
 public class IndexController {
--- security/src/main/java/com/andy/zhflow/security/config/SecurityStarterAutoConfigure.java
+++ security/src/main/java/com/andy/zhflow/security/config/SecurityStarterAutoConfigure.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -19,13 +18,9 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(jsr250Enabled = true)
 public class SecurityStarterAutoConfigure {
     @Bean
     public static PasswordEncoder passwordEncoder(){
@@ -45,6 +40,8 @@ public class SecurityStarterAutoConfigure {
         http.cors().configurationSource(corsConfigurationSource());
 
         http.authorizeHttpRequests((authorize) -> authorize
+                .requestMatchers("/api/admin/index/**").permitAll()
+                .requestMatchers("/api/admin/website/**").permitAll()
                 .requestMatchers("/**").hasAnyRole("admin", "user")
         );
 
