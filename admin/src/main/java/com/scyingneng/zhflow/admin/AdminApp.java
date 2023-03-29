843a76d22b358df97888406fdab349f3f9c96083
Andy <Andy.8866@outlook.com>
2023-03-29 14:55:08 +0800

    Revert "native"
    This reverts commit 3d86c003e04fd665f4b83f00e3329340ab10dab2.

--- admin/src/main/java/com/scyingneng/zhflow/admin/AdminApp.java
+++ admin/src/main/java/com/scyingneng/zhflow/admin/AdminApp.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(proxyBeanMethods = false)
+@SpringBootApplication
 @ComponentScan("com.scyingneng.zhflow.*")
 public class AdminApp {
     public static void main(String[] args) {
