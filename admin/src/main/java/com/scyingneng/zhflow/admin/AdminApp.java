3d86c003e04fd665f4b83f00e3329340ab10dab2
Andy <Andy.8866@outlook.com>
2023-03-29 14:50:45 +0800

    native

--- admin/src/main/java/com/scyingneng/zhflow/admin/AdminApp.java
+++ admin/src/main/java/com/scyingneng/zhflow/admin/AdminApp.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(proxyBeanMethods = false)
 @ComponentScan("com.scyingneng.zhflow.*")
 public class AdminApp {
     public static void main(String[] args) {
